784075819699c58987d3e59efcb93f09d7c7e1ca
Saad <saadnajmdev@gmail.com>
2026-05-10 00:52:49 +0100

    fix images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
     images:{
       remotePatterns:[{protocol:'https',hostname:'covers.openlibrary.org',pathname:'/b/**'}],
+      unoptimized: true,
     }
 };
 
